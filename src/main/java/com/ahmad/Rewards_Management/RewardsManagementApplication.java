2c700ccc37d07ba0351229b6c815cb960def655f
Ahmadbuba <ahmadbubacherryfield@gmail.com>
2024-09-08 19:38:34 +0100

    project start

---
+++ src/main/java/com/ahmad/Rewards_Management/RewardsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ahmad.Rewards_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahmad/Rewards_Management/RewardsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmad.Rewards_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
